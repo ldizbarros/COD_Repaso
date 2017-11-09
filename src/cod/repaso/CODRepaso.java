1424d25fdb3ff1fb847048cdc2aaa6067069c6e2
ldizbarros <ldizbarros@victoria13.danielcastelao.org>
2017-11-09 09:03:51 +0100

    Inicio Proyecto

---
+++ src/cod/repaso/CODRepaso.java
@@ -0,0 +1,16 @@
+package cod.repaso;
+
+import java.util.Scanner;
+
+public class CODRepaso {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println("Introduce tu nombre :)");
+        String nombre = sc.next();
+        
+        System.out.println("Hola "+nombre);
+    }
+    
+}
